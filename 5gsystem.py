340d1eb2e0c4a09f0b22ec4f2d93a61c9fd811a8
sathyanarayananan96 <62218358+sathyanarayananan96@users.noreply.github.com>
2021-03-11 14:08:52 +0530

    Update 5gsystem.py

--- 5gsystem.py
+++ 5gsystem.py
@@ -1,9 +1,17 @@
 #Main program for 5G system
+
+#Import statements:
+#These statements arer used to interact with the tx and rx class of users 1 and 2.
+
 import satscr as user1
 import satscr2 as user2
+
+#math library imported for using basic trigonometric functions to be seen below:
 import math
 
-
+#Radio Access Network- Transmits signal from user 1 to user 2 and vice versa
+#Txuser1, txuser2, rxuser1 & rxuser2 represent the transmitted and received signals of user 1 and 2, which will be 
+#used by the CoreNet for further analysis
 class Ran:
     ipt1 = int(input('enter no usr1- 1:call/text 2:InternetUse 3:Network OS'))
     ipt2 = int(input('enter no usr2- 1:Call/text 2:InternetUse 3:Network OS'))
@@ -12,14 +20,18 @@ class Ran:
     rxuser1 = user1.Receiver.rxfn(txuser2)
     rxuser2 = user2.Receiver.rxfn(txuser1)
 
-
+#Core Network- Interprets signals sent to the users through the RAN, instructs Applayer accordingly
+#Two static methods have been declared to interpret signals for user1 and user2 separately.
 class CoreNet:
-
+#cnbackendu1- corenetwork's backend for user1. (performs background operations)
     @staticmethod
     def cnbackendu1():
+        # Calling class RAN to use tx and rx variables mentioned earlier in lines 16-21
         rn = Ran()
         x = range(360)
         out1 = 0
+        #Iteratively compare each point of the waveform with a standard sin, cos and tan wave, and return op variable
+        #for user1 to applayer, which then displays a message to user1 accordingly
         for i in x:
             if rn.txuser1[i] == math.sin(math.radians(i)):
                 out1 = 1
@@ -28,7 +40,7 @@ class CoreNet:
             elif rn.txuser1[i] == math.tan(math.radians(i)):
                 out1 = 3
         return out1
-
+#Corenetwork backend for user2- same functions as in the case of user1. out2 is the op variable for user2, which helps applayer act accordingly
     @staticmethod
     def cnbackendu2():
         rn = Ran()
@@ -43,19 +55,23 @@ class CoreNet:
                 out2 = 3
         return out2
 
-
+#Application Layer- Accesses the output variables of corenet to decide what to do
 class AppLayer:
+    #Accessing corenet functions mentioned above lines 27-56
     op1 = CoreNet.cnbackendu1()
     op2 = CoreNet.cnbackendu2()
+    #Variables which define user interaction with the system.
     check1 = 0
     check2 = 0
     sendu1 = ''
     sendu2 = ''
     paswd1 = 0
     paswd2 = 0
+    #Screen lock or PIN for smartphones of users 1 and 2. Ideas for better security measures are greatly appreciated 
     paswdchk1 = 1234
     paswdchk2 = 5678
 
+    #Decision mechanism of Applayer, based on corenet inputs for user 1 and 2. Output directly displayed on common screen
     if op1 == 1:
         print('sin wave- call and text user1')
         check1 = int(input('Enter 1 for call and 2 for text'))
