69d8e2f4a1d80f2f3aec543dccb3306ac52edda2
sathyanarayananan96 <62218358+sathyanarayananan96@users.noreply.github.com>
2021-03-11 14:32:04 +0530

    Update user1.py

--- user1.py
+++ user1.py
@@ -1,4 +1,6 @@
 # Program for user 1
+
+#Import statements
 import math
 import numpy as np
 import csv
@@ -6,7 +8,8 @@ import webbrowser
 import sounddevice as sd
 import matplotlib.pyplot as plt
 
-
+#Transmitter class- contains transmit function, with input ipvar representing user1's connection to the 5G network
+#txfn generates the sinusoidal waveforms to be sent to user2 through the 5G network
 class Transmitter:
 
     @staticmethod
@@ -24,26 +27,32 @@ class Transmitter:
                 sig[k] = math.tan(math.radians(x[k]))
         return sig
 
-
+#Receiver class- Contains 5 functions, to be described with inputs from the 5G system through user2
 class Receiver:
 
+    #Represents a steady connection- the function of the RAN is shown to the user
     @staticmethod
     def rxfn(lstvar1):
         print('Connection established with user2')
         return lstvar1
 
+    #Responsible for receiving text message from user 2 through 5G system and displaying to user1
     @staticmethod
     def texting(lstvar2):
         print('Hello User1, you have a new message from user2')
         print(lstvar2)
         return lstvar2
 
+    #This function represents mobile data usage: I've used the simple webbrowser library to open a browser page like google chrome from python, on the command
+    #of the users through the 5G system.
     @staticmethod
     def u1internet(lstvar3):
         new = 2
         webbrowser.open(lstvar3, new=new)
         return lstvar3
 
+    #Network Operating system- my version of it. It provides the services of the network service provider in my case
+    #Allows us to modify our address and contact info, and other plan information, and updates a csv file. Shows o/p to user
     @staticmethod
     def netos(lstvar4):
         if lstvar4 == 1:
